9952993801457ea9b7f94c9674ed836db2f69d19
Patrik <me@patrikbey.com>
2023-10-23 19:00:54 +0200

    Update RegistrationExperiment.sh

--- Processing/Code/RegistrationExperiment.sh
+++ Processing/Code/RegistrationExperiment.sh
@@ -2,4 +2,61 @@
 
 
 
-docker run
+
+# adjust for demos:fsl container name
+docker run -it ale:dev bash
+
+
+
+#################################################
+#                                               #
+#              CREATE VARIABLES                 #
+#                                               #
+#################################################
+
+
+# define input image
+Input="/data/input.nii.gz"
+
+# define reference image
+Reference="/data/MNI.nii.gz"
+
+# define atlas in reference space
+Atlas="/data/HCPMMP1.nii.gz"
+
+# defie output filenames
+
+OutputImage="/data/In2Ref.nii.gz"
+OutputTransformation="/data/In2Ref.mat"
+
+# get some general information about input images
+
+fslinfo ${Input}
+fslinfo ${Reference}
+fslinfo ${Atlas}
+
+
+# get familiar with linear co-registration function FSL-flirt
+flirt
+
+
+#################################################
+#                                               #
+#               DO COMPUTATIONS                 #
+#                                               #
+#################################################
+
+
+flirt \
+    -in ${Input} \
+    -ref ${Reference} \
+    -omat ${OutputTransformation} \
+    -out ${OutputImage}
+
+
+# create atlas in subject space
+# 
+
+flirt \
+    -applyxfm \
+    
